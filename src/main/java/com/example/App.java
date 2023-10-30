b5554a60211c699b3b496e6caf6d0c330f11e67f
kdesh2001 <kdeshpande2001@gmail.com>
2023-10-30 18:35:00 +0530

    Updated the code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,69 @@
 package com.example;
+import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, this is a calculator project!" );
+        System.out.println("Welcome to the Calculator!");
+        Scanner inp=new Scanner(System.in);
+        int run=1;
+        int opcode=-1;
+        do{
+            System.out.println("0. Exit");
+            System.out.println("1. Square root");
+            System.out.println("2. log10");
+            System.out.println("3. Power");
+            System.out.println("4. Factorial");
+            System.out.println("Choose the operation:");
+            opcode=inp.nextInt();
+            if(opcode==0){
+                run=0;
+            }
+            else if(opcode==1){
+                System.out.println("Enter the number:");
+                double num=inp.nextDouble();
+                System.out.println("Answer is:");
+                System.out.println(sqrt(num));
+            }
+            else if(opcode==2){
+                System.out.println("Enter the number:");
+                double num=inp.nextDouble();
+                System.out.println("Answer is:");
+                System.out.println(log10(num));
+            }
+            else if(opcode==3){
+                System.out.println("Enter the first number:");
+                int num1=inp.nextInt();
+                System.out.println("Enter the power:");
+                int num2=inp.nextInt();
+                System.out.println("Answer is:");
+                System.out.println(power(num1, num2));
+            }
+            else if(opcode==4){
+                System.out.println("Enter the number:");
+                int num=inp.nextInt();
+                System.out.println("Answer is:");
+                System.out.println(fact(num));
+            }
+
+        }while(run==1);
+        inp.close();
+    }
+    public static double sqrt(double num){
+        return Math.sqrt(num);
+    }
+    public static double log10(double num){
+        return Math.log10(num);
+    }
+    public static double power(int num1, int num2){
+        return Math.pow(num1,num2);
+    }
+    public static int fact(int num){
+        int ans=1;
+        for(int i=1;i<=num;i++){
+            ans=ans*i;
+        }
+        return ans;
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,20 +1,40 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test_sqrt() {
+        double val = 36.0;
+        double expectedResult = 6.0;
+        double result = App.sqrt(val);
+        Assert.assertEquals(expectedResult, result, 0.0f);
+    }
+
+    @Test
+    public void test_fact() {
+        int a = 7;
+        int expectedResult = 5040;
+        int result = App.fact(a);
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void test_log10() {
+        double a = 100.0;
+        double expectedResult = 2.0;
+        double result = App.log10(a);
+        Assert.assertEquals(expectedResult, result, 0.2f);
+    }
+
+    @Test
+    public void test_power() {
+        int a = 3;
+        int b = 4;
+        int expectedResult = 81;
+        double result = App.power(a,b);
+        Assert.assertEquals(expectedResult, result, 0.0f);
     }
 }
